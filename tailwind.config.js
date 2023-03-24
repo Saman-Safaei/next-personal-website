688bbc2ea8fe84b84108be7395fbdd656c37ce20
Saman-Safaei <xsamansafaeix@gmail.com>
2023-03-24 19:18:55 +0430

    feat: generate tailwind config and tailwind class

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          lighter: "#6AB8F7",
+          light: '#3AA1F3',
+          DEFAULT: '#0F8AEB',
+          dark: "#0D6EBA",
+          darker: "#0B528A"
+        }
+      }
+    },
   },
   plugins: [],
 }
