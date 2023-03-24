a0a6fc07a84ad74cfeecfbbe917b6b84ef9a06b5
Saman-Safaei <xsamansafaeix@gmail.com>
2023-03-24 19:18:26 +0430

    feat: add font to body and make document rtl

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
+import { Vazirmatn } from "next/font/google";
+
+const vazirmatn = Vazirmatn();
 
 export default function Document() {
   return (
-    <Html lang="fa">
+    <Html lang="fa" dir="rtl">
       <Head />
-      <body>
+      <body className={vazirmatn.className}>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
