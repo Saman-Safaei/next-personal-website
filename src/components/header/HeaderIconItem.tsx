b397b0587929a25dc6e64f73d6626ec3f701d300
Saman-Safaei <xsamansafaeix@gmail.com>
2023-03-25 22:53:56 +0430

    feat: create an icon item

---
+++ src/components/header/HeaderIconItem.tsx
@@ -0,0 +1,26 @@
+import type { FC } from 'react';
+import Image from 'next/image';
+
+export interface HeaderIconItem {
+  link: string;
+  logo: string;
+  alt: string;
+  className?: string;
+  priority?: boolean;
+}
+
+const HeaderIconItem: FC<HeaderIconItem> = ({
+  alt,
+  logo,
+  link,
+  className = 'w-8 h-8',
+  priority = false,
+}) => (
+  <li>
+    <a href={link}>
+      <Image className={className} src={logo} alt={alt} priority={priority} />
+    </a>
+  </li>
+);
+
+export default HeaderIconItem;
