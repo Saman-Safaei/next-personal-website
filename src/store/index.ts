bdf6ea531c517caec1e060968a3ccacd70b26380
Saman-Safaei <xsamansafaeix@gmail.com>
2023-04-01 00:54:22 +0430

    feat: create a store with redux toolkit

---
+++ src/store/index.ts
@@ -0,0 +1,20 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { TypedUseSelectorHook, useSelector, useDispatch } from 'react-redux';
+import uiSlice from '@/store/ui-slice';
+
+const createStore = () => {
+  return configureStore({
+    reducer: {
+      [uiSlice.name]: uiSlice.reducer,
+    },
+  });
+};
+
+export type StoreType = ReturnType<typeof createStore>
+export type StoreState = ReturnType<StoreType['getState']>;
+export type AppDispatch = StoreType['dispatch'];
+
+export const useSelect: TypedUseSelectorHook<StoreState> = useSelector;
+export const useAppDispatch: () => AppDispatch = useDispatch;
+
+export default createStore;
