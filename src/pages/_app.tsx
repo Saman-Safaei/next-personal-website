d41108bc12ddea47b4a1025efc3368183fa3eb1b
Saman-Safaei <xsamansafaeix@gmail.com>
2023-04-01 00:55:30 +0430

    feat: wrap app with redux store

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,23 @@
-import "@/styles/globals.css";
-import type {AppProps} from "next/app";
-import type {NextPage} from "next";
-import type {ReactElement, ReactNode} from "react";
+import type { AppProps } from 'next/app';
+import type { NextPage } from 'next';
+import type { ReactElement, ReactNode } from 'react';
+import { useState } from 'react';
+import { Provider } from 'react-redux';
+import createStore from '../store';
+import '@/styles/globals.css';
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
-  getLayout?: (page: ReactElement) => ReactNode
-}
+  getLayout?: (page: ReactElement) => ReactNode;
+};
 type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout;
-}
+};
 
-export default function App({Component, pageProps}: AppPropsWithLayout) {
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  const [store] = useState(() => createStore());
   const getLayout = Component.getLayout || ((page: any) => page);
-  return getLayout(<Component {...pageProps} />);
+
+  return (
+    <Provider store={store}>{getLayout(<Component {...pageProps} />)}</Provider>
+  );
 }
