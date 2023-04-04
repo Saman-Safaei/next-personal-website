7eb1ed3738adefb03245691f682b24e1ab5469c0
Saman-Safaei <xsamansafaeix@gmail.com>
2023-04-04 21:48:18 +0430

    fix: remove unused codes

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,15 +1,7 @@
 import type { NextPageWithLayout } from '@/pages/_app';
-import { useAppDispatch, useSelect } from '@/store';
-import { actions } from '@/store/ui-slice';
-import { Fragment } from 'react';
 import DefaultLayout from '@/components/layout/DefaultLayout';
 
 const Blog: NextPageWithLayout = () => {
-  const isSidebarOpen = useSelect(state => state.ui.isSidebarOpen);
-  const dispatch = useAppDispatch();
-
-  const toggleClickHandler = () => dispatch(actions.toggleSidebar());
-
   return (
     <div className='py-20'>
       <p className='text-center w-fit mx-auto text-2xl mb-3 animated-text'>... به زودی در دسترس قرار می گیرد ...</p>
