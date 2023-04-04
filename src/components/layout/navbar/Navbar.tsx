016e3521a9b67e4bd14e5982ff801fe8a938eef2
Saman-Safaei <xsamansafaeix@gmail.com>
2023-04-04 22:17:16 +0430

    feat: move scroll logic to a hook

--- src/components/layout/navbar/Navbar.tsx
+++ src/components/layout/navbar/Navbar.tsx
@@ -3,25 +3,17 @@ import { Fragment, useCallback, useEffect, useRef, useState } from 'react';
 import { Josefin_Sans } from 'next/font/google';
 import NavbarLink from '@/components/layout/navbar/NavbarLink';
 import { CSSTransition, SwitchTransition } from 'react-transition-group';
+import useScrollData from '@/hooks/useScrollData';
 
 const josefin = Josefin_Sans({ subsets: ['latin'] });
 
 const Navbar: FC = () => {
-  const [fixed, setFixed] = useState(false);
   const navbarRef = useRef(null);
+  const [fixed] = useScrollData(300);
 
-  const onScrollHandler = useCallback(() => {
-    const scroll = 300;
-    if (window.scrollY > scroll && !fixed) setFixed(true);
-    else if (window.scrollY <= scroll && fixed) setFixed(false);
-  }, [fixed]);
-
-  useEffect(() => {
-    document.addEventListener('scroll', onScrollHandler);
-    return () => document.removeEventListener('scroll', onScrollHandler);
-  }, [onScrollHandler]);
-
-  const dynamicClasses = fixed ? 'fixed top-0 right-0 bg-gray-700/50' : '-mt-navbar border-solid border-b border-b-gray-700/70';
+  const dynamicClasses = fixed
+    ? 'fixed top-0 right-0 bg-gray-700/50'
+    : '-mt-navbar border-solid border-b border-b-gray-700/70';
 
   return (
     <Fragment>
@@ -41,9 +33,19 @@ const Navbar: FC = () => {
                 <NavbarLink url='/'>صفحه اصلی</NavbarLink>
                 <NavbarLink url='/blog'>وبلاگ</NavbarLink>
               </ul>
-              <button className="block lg:hidden w-8 h-8">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-full h-full">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25" />
+              <button className='block lg:hidden w-8 h-8'>
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  fill='none'
+                  viewBox='0 0 24 24'
+                  strokeWidth={1.5}
+                  stroke='currentColor'
+                  className='w-full h-full'>
+                  <path
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    d='M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25'
+                  />
                 </svg>
               </button>
               <p className={`${josefin.className} font-bold text-lg`}>
